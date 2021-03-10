babacf5af739053a3244a99f44c84600cb89e9a7
mozhimen <mozhimen@163.com>
2021-03-10 14:59:53 +0800

    310

--- HomePage/js/main.js
+++ HomePage/js/main.js
@@ -26,6 +26,38 @@
 		$("#loading").fadeOut(2000);
 	});
 	
+	//首页轮播图
+	$(document).ready(function () {
+	  $('.owl-one').owlCarousel({
+	    loop: true,
+	    margin: 0,
+	    nav: false,
+	    responsiveClass: true,
+	    autoplay: true,
+	    autoplayTimeout: 5000,
+	    autoplaySpeed: 1000,
+	    autoplayHoverPause: false,
+	    responsive: {
+	      0: {
+	        items: 1,
+	        nav: false
+	      },
+	      480: {
+	        items: 1,
+	        nav: false
+	      },
+	      667: {
+	        items: 1,
+	        nav: true
+	      },
+	      1000: {
+	        items: 1,
+	        nav: true
+	      }
+	    }
+	  })
+	});
+	
 	// Full Height
 	var fullHeight = function() {
 		if (!isMobile.any()) {
